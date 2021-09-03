8d5b749108229fe4eddb1f5b7b4b0f4a0416114b
Dmitry Nefedov <nefedovds@ya.ru>
2021-09-03 12:29:46 +0300

    Update and rename Office Uninstall/Office Uninstall.cmd to Office_Uninstall/Office_Uninstall.cmd

--- Office_Uninstall/Office_Uninstall.cmd
+++ Office_Uninstall/Office_Uninstall.cmd
@@ -1,4 +1,4 @@
 reg add "HKLM\SOFTWARE\Microsoft\Windows Script Host\Settings" /v Enabled /t REG_DWORD /d 1 /f
 cscript.exe "%~dp0Office Uninstall.vbs" ALL /Quiet /NoCancel /Force /OSE
-reg add "HKLM\SOFTWARE\Microsoft\Windows Script Host\Settings" /v Enabled /t REG_DWORD /d 0 /f
+
 pause
