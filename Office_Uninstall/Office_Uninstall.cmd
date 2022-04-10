912a0b3dad9b42d32b965aea0f58e557c06157c5
Dmitry Nefedov <nefedovds@ya.ru>
2022-04-10 15:52:45 +0300

    Update Office_Uninstall.cmd

--- Office_Uninstall/Office_Uninstall.cmd
+++ Office_Uninstall/Office_Uninstall.cmd
@@ -1,4 +1,4 @@
 reg add "HKLM\SOFTWARE\Microsoft\Windows Script Host\Settings" /v Enabled /t REG_DWORD /d 1 /f
-cscript.exe "%~dp0Office_Uninstall.vbs" ALL /Quiet /NoCancel /Force /OSE
+cscript.exe "%~dp0Office_Uninstall.vbs" /Force
 
 pause
